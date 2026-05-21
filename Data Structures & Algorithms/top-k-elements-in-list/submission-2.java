a9734ab62120945a8d487f0e7aff1b81df28386a
Tushar1235 <60726488+Tushar1235@users.noreply.github.com>
2026-05-21 16:28:54 +0530

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new TreeMap<>();
+
+        for(int i: nums) {
+            map.put(i, map.getOrDefault(i,0)+1);
+        }
+
+        return map.entrySet().stream().
+        sorted(Comparator.comparingLong(Map.Entry<Integer,Integer>::getValue)
+        .reversed()).limit(k).mapToInt(keys-> keys.getKey()).toArray();
+
+    }
+}
